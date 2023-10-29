7c48b2f7b658272c57864a7b9cc5c18989bb3768
Olavo Dias <olavodias@gmail.com>
2023-10-29 15:37:55 -0600

    Fixed the Project URL in the Nuget Package

--- src/GitHubAuth/AppAuthenticator.cs
+++ src/GitHubAuth/AppAuthenticator.cs
@@ -29,8 +29,6 @@ using System;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Json;
-using System.Reflection.Metadata;
-using System.Threading.Tasks;
 using GitHubAuth.Jwt;
 
 namespace GitHubAuth;
